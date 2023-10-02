32d412abd2f259a8e4e56a2767a08b97f3cb2c0a
Ziyao Guo <103924255+GzyAftermath@users.noreply.github.com>
2023-10-02 12:17:52 +0800

    Delete buffer directory

--- buffer/run_buffer.sh
+++
@@ -1,4 +0,0 @@
-#buffer for FTD 
-CUDA_VISIBLE_DEVICES=1 python buffer_FTD.py --dataset=CIFAR10 --model=ConvNet --train_epochs=50 --num_experts=100 --zca \
---buffer_path=../buffer_storage/ --data_path=../dataset/ \
---rho_max=0.01 --rho_min=0.01 
